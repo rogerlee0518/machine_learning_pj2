16cc516fad492766bfe4ddc572667062b8d1ae18
Calvin <chlee4@timberlake.cse.buffalo.edu>
2015-04-08 16:48:48 -0400

    first commit

---
+++ script.py
@@ -0,0 +1,117 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.io import loadmat
+from math import sqrt
+import scipy.io
+import matplotlib.pyplot as plt
+import pickle
+
+def learnOLERegression(X,y):
+    # Inputs:                                                         
+    # X = N x d 
+    # y = N x 1                                                               
+    # Output: 
+    # w = d x 1                                                                
+    # IMPLEMENT THIS METHOD 
+    # w = (X.T*X)^-1 * X.T * y (lec.22)  
+    print(X.shape[1]);
+    print(y.shape[1]);                                     
+    A = np.linalg.inv(np.dot(X.T, X));
+    print(A.shape[0]);
+    B = np.dot(A, X.T);
+    print(B.shape[0]);
+    w = np.dot(B, y);
+    print(w.shape[1]);
+    return w;
+
+
+def testOLERegression(w,Xtest,ytest):
+    # Inputs:
+    # w = d x 1
+    # Xtest = N x d
+    # ytest = X x 1
+    # Output:
+    # rmse
+    # IMPLEMENT THIS METHOD
+    # J(w) = sqrt(sum((y-X*w)^2))*(1/N)
+    A = ytest - np.dot(Xtest, w);       #Difference between y and x*w
+    B = np.square(A);                   #Power of 2
+    C = np.sum(B);                      #Add the summation to N
+    D = np.sqrt(C);                     #Square root the function
+    rmse = D/Xtest.shape[0];            #Divide result by N
+    return rmse
+
+# Main script
+'''
+# Problem 1
+# load the sample data                                                                 
+X,y,Xtest,ytest = pickle.load(open('sample.pickle','rb'),encoding = 'latin1')            
+
+# LDA
+means,covmat = ldaLearn(X,y)
+ldaacc = ldaTest(means,covmat,Xtest,ytest)
+print('LDA Accuracy = '+str(ldaacc))
+# QDA
+means,covmats = qdaLearn(X,y)
+qdaacc = qdaTest(means,covmats,Xtest,ytest)
+print('QDA Accuracy = '+str(qdaacc))
+'''
+# Problem 2
+
+X,y,Xtest,ytest = pickle.load(open('diabetes.pickle','rb'))   
+# add intercept
+X_i = np.concatenate((np.ones((X.shape[0],1)), X), axis=1)
+Xtest_i = np.concatenate((np.ones((Xtest.shape[0],1)), Xtest), axis=1)
+
+w = learnOLERegression(X,y)
+mle = testOLERegression(w,Xtest,ytest)
+
+w_i = learnOLERegression(X_i,y)
+mle_i = testOLERegression(w_i,Xtest_i,ytest)
+
+print('RMSE without intercept '+str(mle))
+print('RMSE with intercept '+str(mle_i))
+'''
+# Problem 3
+k = 21
+lambdas = np.linspace(0, 0.004, num=k)
+i = 0
+rmses3 = np.zeros((k,1))
+for lambd in lambdas:
+    w_l = learnRidgeRegression(X_i,y,lambd)
+    rmses3[i] = testOLERegression(w_l,Xtest_i,ytest)
+    i = i + 1
+plt.plot(lambdas,rmses3)
+
+# Problem 4
+k = 21
+lambdas = np.linspace(0, 0.004, num=k)
+i = 0
+rmses4 = np.zeros((k,1))
+opts = {'maxiter' : 100}    # Preferred value.                                                
+w_init = np.zeros((X_i.shape[1],1))
+for lambd in lambdas:
+    args = (X_i, y, lambd)
+    w_l = minimize(regressionObjVal, w_init, jac=True, args=args,method='CG', options=opts)
+    w_l_1 = np.zeros((X_i.shape[1],1))
+    for j in range(len(w_l.x)):
+        w_l_1[j] = w_l.x[j]
+    rmses4[i] = testOLERegression(w_l_1,Xtest_i,ytest)
+    i = i + 1
+plt.plot(lambdas,rmses4)
+
+
+# Problem 5
+pmax = 7
+lambda_opt = lambdas[np.argmin(rmses4)]
+rmses5 = np.zeros((pmax,2))
+for p in range(pmax):
+    Xd = mapNonLinear(X[:,2],p)
+    Xdtest = mapNonLinear(Xtest[:,2],p)
+    w_d1 = learnRidgeRegression(Xd,y,0)
+    rmses5[p,0] = testOLERegression(w_d1,Xdtest,ytest)
+    w_d2 = learnRidgeRegression(Xd,y,lamda_opt)
+    rmses5[p,1] = testOLERegression(w_d2,Xdtest,ytest)
+plt.plot(range(pmax),rmses5)
+plt.legend(('No Regularization','Regularization'))
+'''
